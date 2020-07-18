9b255d95e4de54f08215a6d97a26d47fb88ab2cf
bilgetatar <bt96@duke.edu>
2020-07-19 02:54:32 +0300

    forum changes made

--- LoremIpsum/ForumViewController.swift
+++ LoremIpsum/ForumViewController.swift
@@ -7,24 +7,69 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
-class ForumViewController: UIViewController {
+class ForumViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var text: UITextField!
+    @IBOutlet weak var tView: UITableView!
+    
+    @IBAction func buttonClicked(_ sender: Any) {
+        ref?.child("Posts").childByAutoId().setValue(text.text);
+    }
+    
+    var postData = [String]()
+    var ref: DatabaseReference?
+    var refHandle:DatabaseHandle?
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tView.separatorStyle = UITableViewCell.SeparatorStyle.none
 
-        // Do any additional setup after loading the view.
+        
+        tView.dataSource = self
+        tView.delegate = self
+        ref = Database.database().reference()
+
+        refHandle = ref?.child("Posts").observe(.childAdded, with: { (snapshot) in
+            let post = snapshot.value as? String
+            print(snapshot.value)
+            
+            if let actualPost = post {
+                print("Yes")
+                self.postData.append(actualPost)
+                self.tView.reloadData()
+            }
+          // ...
+        })
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         // #warning Incomplete implementation, return the number of rows
+         return postData.count;
+     }
+     
+     func numberOfSections(in tableView: UITableView) -> Int {
+         // #warning Incomplete implementation, return the number of sections
+         return 1;
+     }
+     
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+         return 5;
+     }
+     
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath)
+         cell.textLabel!.numberOfLines = 0
+         cell.textLabel!.lineBreakMode = .byWordWrapping
+         cell.textLabel!.font = UIFont.systemFont(ofSize: 14.0)
+         cell.textLabel?.text = postData[indexPath.row]
+         cell.layer.masksToBounds = true
+         cell.layer.cornerRadius = 5
+         cell.layer.borderWidth = 2
+         cell.layer.shadowOffset = CGSize(width: -1, height: 1)
+         
+         return cell;
+         
+     }
 }
